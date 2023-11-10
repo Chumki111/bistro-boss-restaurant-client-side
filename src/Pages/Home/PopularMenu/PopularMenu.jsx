f2453e5dfca784efe6813417246eca72b07294e6
Chumki khatun <chumkikhatun370@gamil.com>
2023-11-10 12:37:15 +0600

    banner section and OrderSlider section

--- src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,20 +1,36 @@
+import { useEffect, useState } from "react";
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+import MenuItem from "../../../Shared/MenuItem/MenuItem";
 
 
 const PopularMenu = () => {
+     const [menus,setMenus]  = useState([])
+    useEffect(() =>{
+        fetch('menu.json')
+        .then(res => res.json())
+        .then(data =>{
+            const popularItems = data.filter(item =>item.category === 'popular')
+            setMenus(popularItems)
+        } )
+    },[])
+    console.log(menus);
     return (
-        <div className="">
-           <div>
+        <section className="">
+           
            <SectionTitle 
             subHeading={'---Check it out---'}
             heading={'FROM OUR MENU'}
             ></SectionTitle>
-           </div>
+           
 
            <div className='px-2 md:px-8 lg:px-14'>
-
+                <div>
+                    {
+                        menus?.map(menu => <MenuItem menu={menu} key={menu._id}></MenuItem>)
+                    }
+                </div>
            </div>
-        </div>
+        </section>
     );
 };
 
---
+++ src/Shared/MenuItem/MenuItem.jsx
@@ -0,0 +1,13 @@
+import PropTypes from 'prop-types';
+
+const MenuItem = ({menu}) => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+MenuItem.propTypes ={
+    menu : PropTypes.menu
+}
+export default MenuItem;
